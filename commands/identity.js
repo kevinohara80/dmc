d7ef9646e0b1835ce1af9320d3d2a1a5bbb06edc
Kevin OHara <kevinohara80@gmail.com>
2014-11-07 15:27:13 -0500

    adding identity

--- commands/identity.js
+++ commands/identity.js
@@ -2,13 +2,27 @@ var user     = require('../lib/user');
 var logger   = require('../lib/logger');
 var cliUtil  = require('../lib/cli-util');
 var sfClient = require('../lib/sf-client');
+var _        = require('lodash');
+
+
+function flatten(obj, fields, pre) {
+  _.forIn(obj, function(v, k) {
+    if(pre) k = pre + '.' + k;
+    if(_.isObject(v)) {
+      flatten(v, fields, k);
+    } else if(!fields || !fields.length || fields.indexOf(k) !== -1) {
+      logger.list(k + ': ' + v);
+    }
+  });
+}
 
 var run = module.exports.run = function(org, opts, cb) {
   var oauth = user.getCredential(org);
 
   sfClient.getIdentity({ oauth: oauth }, function(err, res){
     if(err) return cb(err);
-    console.log(res);
+    if(opts.fields) opts.fields = opts.fields.split(',');
+    flatten(res, opts.fields);
     cb();
   });
 };
@@ -16,7 +30,8 @@ var run = module.exports.run = function(org, opts, cb) {
 module.exports.cli = function(program) {
   program.command('identity <org>')
     .description('show the identity for target <org>')
-    .action(function(org, file, opts) {
+    .option('-f, --fields <fields>', 'Comma-separated fields to show. Use dot notation')
+    .action(function(org, opts) {
       cliUtil.checkForOrg(org);
       run(org, opts, cliUtil.callback);
     });

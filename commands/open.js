d2ec396822ec03c5740051e940cba1745c7f8042
Kevin OHara <kevinohara80@gmail.com>
2014-11-07 16:27:21 -0500

    fixes open to call getIdentity

--- commands/open.js
+++ commands/open.js
@@ -2,20 +2,31 @@ var user     = require('../lib/user');
 var logger   = require('../lib/logger');
 var cliUtil  = require('../lib/cli-util');
 var sfClient = require('../lib/sf-client');
+var identity = require('./identity');
 var spawn    = require('child_process').spawn;
 
 var run = module.exports.run = function(org, opts, cb) {
+
   var oauth = user.getCredential(org);
 
-  var url = oauth.instance_url + '/secur/frontdoor.jsp?sid=' + oauth.access_token
+  sfClient.getIdentity({ oauth: oauth }, function(err, res) {
+    if(err) return cb(err);
 
-  logger.log('opening: ' + org);
-  
-  var open = spawn('open', [url]);
+    logger.log('logging in: ' + res.username);
 
-  open.on('exit', function(code, sig) {
-    logger.log('open command complete');
-    cb();
+    oauth = user.getCredential(org);
+
+    var url = oauth.instance_url + '/secur/frontdoor.jsp?sid=' + oauth.access_token
+
+    logger.log('opening: ' + org);
+
+    var open = spawn('open', [url]);
+
+    open.on('exit', function(code, sig) {
+      logger.log('open command complete');
+      cb();
+    });
+
   });
 
 };

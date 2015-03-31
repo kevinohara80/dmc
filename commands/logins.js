b3370a9f6e216245c2d082f073335073179659c3
Kevin O'Hara <kevinohara80@gmail.com>
2015-03-31 10:26:14 -0400

    refactoring logins

--- commands/logins.js
+++ commands/logins.js
@@ -1,28 +1,45 @@
-var logger = require('../lib/logger');
-var user   = require('../lib/user');
+var logger  = require('../lib/logger');
+var user    = require('../lib/user');
+var cliUtil = require('../lib/cli-util.js');
 
 
-var run = module.exports.run = function(opts) {
-  var creds = user.listCredentials();
+var run = module.exports.run = function(opts, cb) {
+  // var creds = user.listCredentials();
+  //
+  // if(!creds || creds.length === 0) {
+  //   logger.log('no logins found!');
+  //   return logger.done();
+  // }
+  //
+  // logger.log('listing all logins:');
+  //
+  // creds.forEach(function(c) {
+  //   logger.list(c.name + ' - ' + c.instance_url);
+  // });
+  //
+  // logger.done();
 
-  if(!creds || creds.length === 0) {
-    logger.log('no logins found!');
-    return logger.done();
-  }
+  user.listCredentials().then(function(creds) {
+    if(!creds || creds.length === 0) {
+      logger.log('no logins found!');
+      return cb();
+    }
 
-  logger.log('listing all logins:');
+    logger.log('listing all logins:');
 
-  creds.forEach(function(c) {
-    logger.list(c.name + ' - ' + c.instance_url);
+    creds.forEach(function(c) {
+      logger.list(c.name + ' - ' + c.instance_url);
+    });
+    cb();
+  }).catch(function(err) {
+    throw err;
   });
-
-  logger.done();
 };
 
 var cli = module.exports.cli = function(program) {
   program.command('logins')
     .description('list your salesforce logins')
     .action(function(opts) {
-      run(opts);
+      run(opts, cliUtil.callback);
     });
 };

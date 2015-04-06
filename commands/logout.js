7199d86684182f55096defc6e2c2652920681f5d
Kevin O'Hara <kevinohara80@gmail.com>
2015-04-06 08:53:12 -0400

    resolve org

--- commands/logout.js
+++ commands/logout.js
@@ -1,41 +1,38 @@
-var logger   = require('../lib/logger');
-var user     = require('../lib/user');
-var sfClient = require('../lib/sf-client');
-var cliUtil  = require('../lib/cli-util');
-var async    = require('async');
+var logger     = require('../lib/logger');
+var user       = require('../lib/user');
+var sfClient   = require('../lib/sf-client');
+var cliUtil    = require('../lib/cli-util');
+var async      = require('async');
+var resolveOrg = require('../lib/resolve-org');
 
-var run = module.exports.run = function(org, opts, cb) {
+var run = module.exports.run = function(opts, cb) {
 
-  var orgCred = user.getCredential(org);
+  var data = {
+    oauth: opts.oauth,
+    token: opts.oauth.access_token
+  };
 
-  async.series([
-    function(cb) {
-      logger.log('revoking access token');
-      sfClient.revokeToken({ oauth: orgCred, token: orgCred.access_token }, cb);
-    },
-    function(cb) {
-      logger.log('revoking refresh token');
-      sfClient.revokeToken({ oauth: orgCred, token: orgCred.refresh_token }, cb);
-    },
-    function(cb) {
-      logger.log('removing local credentials');
-      user.deleteCredential(org);
-      cb();
-    }
-  ], function(err, results) {
-    if(err) {
-      cb(err.message);
-    } else {
-      cb(null);
-    }
-  });
+  logger.log('revoking access token');
+  sfClient.revokeToken(data).then(function() {
+    data.token = data.oauth.refresh_token;
+    logger.log('revoking refresh token');
+    return sfClient.revokeToken(data);
+  }).then(function(){
+    logger.log('removing local credentials');
+    return user.deleteCredential(opts.oauth.nick);
+  }).then(function(){
+    cb();
+  }).catch(cb);
 };
 
 var cli = module.exports.cli = function(program) {
-  program.command('logout <org>')
+  program.command('logout')
     .description('logout of a Salesforce organization')
-    .action(function(org, opts) {
-      cliUtil.checkForOrg(org);
-      run(org, opts, cliUtil.callback);
+    .option('-o, --org <org>', 'Specify the Salesforce org to log out of')
+    .action(function(opts) {
+      resolveOrg(opts.org).then(function(oauth){
+        opts.oauth = oauth;
+        run(opts, cliUtil.callback);
+      }).catch(cliUtil.callback);
     });
 };

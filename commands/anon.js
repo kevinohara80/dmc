4beea354839e33004246491698a3ca6362b7094e
Kevin O'Hara <kevinohara80@gmail.com>
2015-10-29 11:32:58 -0700

    adding v1 of anon

--- commands/anon.js
+++ commands/anon.js
@@ -1,14 +1,16 @@
+var Promise  = require('bluebird');
 var user     = require('../lib/user');
 var logger   = require('../lib/logger');
 var fs       = require('../lib/fs');
 var cliUtil  = require('../lib/cli-util');
 var sfClient = require('../lib/sf-client');
+var resolve  = require('../lib/resolve');
 
 function loadFile(filePath) {
   return fs.existsAsync(filePath)
     .then(function(exists) {
       if(!exists) {
-        return throw new Error('file does not exist: ' + filePath);
+        throw new Error('file does not exist: ' + filePath);
       }
       return fs.readFileAsync(filePath, { encoding: 'utf8' });
     }).then(function(file) {
@@ -19,23 +21,77 @@ function loadFile(filePath) {
     });
 }
 
-var run = module.exports.run = function(opts, cb) {
-
+function validateOpts(opts) {
   if(!opts.code && !opts.file) {
-    return cb(new Error('You must provide apex code or a file to execute'));
+    throw new Error('You must provide apex code or a file to execute');
   }
 
   if(opts.code && opts.file) {
-    return cb(new Error('You cannot provide code and a file at the same time'));
+    throw new Error('You cannot provide code and a file at the same time');
   }
+}
 
-  return Promise.resolve()
+var run = module.exports.run = function(opts, cb) {
 
-  .then(function() {
-    if()
-  })
+  return resolve(cb, function(){
+    var client;
 
-  cb();
+    validateOpts(opts);
+
+    return sfClient.getClient(opts.oauth)
+
+    .then(function(c) {
+      client = c;
+    })
+
+    .then(function() {
+      if(opts.file) {
+        return loadFile(opts.file);
+      } else {
+        return opts.code;
+      }
+    })
+
+    .then(function(code) {
+      logger.log('executing anonymous code');
+
+      return client.tooling.executeAnonymous({
+        oauth: opts.oauth,
+        code: code
+      });
+    })
+
+    .then(function(res) {
+
+      if(res.success) {
+        logger.log('execution: ' + 'success'.highlight);
+      } else {
+        logger.error('execution: failed');
+      }
+
+      if(res.compiled) {
+        logger.log('compiled: ' + res.compiled);
+      } else {
+        logger.error('compiled: ' + res.compiled);
+      }
+
+      if(!res.success) {
+
+        if(res.compileProblem) {
+          logger.error('compile problem: ' + res.compileProblem);
+        }
+        if(res.exceptionMessage) {
+          logger.error('exception: ' + res.exceptionMessage);
+        }
+        if(res.exceptionStackTrace) {
+          logger.error('exception stack: ' + res.exceptionStackTrace);
+        }
+        throw new Error('execute anonymous failed');
+      }
+    });
+
+  });
+
 };
 
 module.exports.cli = function(program) {

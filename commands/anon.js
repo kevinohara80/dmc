e0422c7aee867847e4838963a1acc0349b101e90
Kevin O'Hara <kevinohara80@gmail.com>
2016-02-18 10:12:10 -0500

    #22 implements anon script argument merging

--- commands/anon.js
+++ commands/anon.js
@@ -1,10 +1,11 @@
-var Promise  = require('bluebird');
-var user     = require('../lib/user');
-var logger   = require('../lib/logger');
-var fs       = require('../lib/fs');
-var cliUtil  = require('../lib/cli-util');
-var sfClient = require('../lib/sf-client');
-var resolve  = require('../lib/resolve');
+var Promise    = require('bluebird');
+var user       = require('../lib/user');
+var logger     = require('../lib/logger');
+var fs         = require('../lib/fs');
+var cliUtil    = require('../lib/cli-util');
+var sfClient   = require('../lib/sf-client');
+var resolve    = require('../lib/resolve');
+var Handlebars = require('handlebars');
 
 function loadFile(filePath) {
   return fs.existsAsync(filePath)
@@ -28,6 +29,10 @@ function validateOpts(opts) {
 
   if(opts.code && opts.file) {
     throw new Error('You cannot provide code and a file at the same time');
+  }
+
+  if(opts.arg && opts.arg._errors && opts.arg._errors.length > 0) {
+    throw new Error('Invalid argument(s): ' + opts.arg._errors.join(', '));
   }
 }
 
@@ -53,11 +58,15 @@ var run = module.exports.run = function(opts, cb) {
     })
 
     .then(function(code) {
+      return Handlebars.compile(code);
+    })
+
+    .then(function(template) {
       logger.log('executing anonymous code');
 
       return client.tooling.executeAnonymous({
         oauth: opts.oauth,
-        code: code
+        code: template(opts.arg || {})
       });
     })
 
@@ -94,14 +103,40 @@ var run = module.exports.run = function(opts, cb) {
 
 };
 
+var collect = function(val, memo) {
+  var parsedVal = val.split('=');
+  if(parsedVal.length != 2) {
+    if(!memo._errors) memo._errors = [];
+    memo._errors.push(val);
+    return memo;
+  }
+  var value = parsedVal[1].split(',');
+  memo[parsedVal[0]] = (value.length > 1) ? value : value[0]; 
+  return memo;
+};
+
 module.exports.cli = function(program) {
   program.command('anon [code]')
     .description('execute anonymous apex [code]')
     .option('-o, --org <org>', 'the Salesforce organization to use')
     .option('-f, --file <file>', 'specify a file that contains apex code')
+    .option('-a, --arg <arg>', 'specify one or more arguments to merge to the script.', collect, {})
     .action(function(code, opts) {
       opts.code = code;
       opts._loadOrg = true;
       return cliUtil.executeRun(run)(opts);
+    })
+    .on('--help', function() {
+      console.log('  Examples:');
+      console.log();
+      console.log('    # inline apex code');
+      console.log('    $ dmc anon "System.debug(\'hello world\');"');
+      console.log();
+      console.log('    # apex code in a file');
+      console.log('    $ dmc anon -f myscript.apex');
+      console.log();
+      console.log('    # merge arguments using handlebars syntax in file');
+      console.log('    $ dmc anon -f myscript.apex -a mode=test -a myarray=foo,bar,baz');
+      console.log();
     });
 };
